d07eb4e2db74cd8bb06eef8f88d030ee7dcc2741
Varsha-Bosch <varshasrinivas46@gmail.com>
2024-09-25 19:43:36 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,14 +6,21 @@
 
 class StringCalculator {
 public:
+    // Main add method
     int add(const std::string& numbers);
 
 private:
-    std::string extractDelimiter(std::string& numbers);
-    std::vector<std::string> split(const std::string& str, const std::string& delimiter);
-    std::vector<int> checkForNegatives(const std::vector<int>& numbers);
+   
+    std::string getDelimiter(const std::string& numbers); 
+    std::string sanitizeInput(const std::string& numbers, const std::string& delimiter); 
+    std::vector<int> convertToNumbers(const std::string& modifiedNumbers, const std::string& delimiter); 
+    void checkAndHandleNegatives(const std::vector<int>& numbers); 
+    int sumIgnoringLargeNumbers(const std::vector<int>& numbers); 
+   
+    std::vector<int> checkForNegatives(const std::vector<int>& numbers); 
     std::string formatNegatives(const std::vector<int>& negatives);
-    int ignoreNumbersGreaterThan1000(const std::vector<int>& numbers);
+    std::string extractDelimiter(std::string& numbers); 
+    std::vector<std::string> split(const std::string& str, const std::string& delimiter); 
 };
 
 #endif // STRINGCALCULATOR_H
