fbc69a59214097af0eef6b38b794e83037d263fc
Varsha-Bosch <varshasrinivas46@gmail.com>
2024-09-25 19:28:56 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,8 +9,8 @@ public:
     int add(const std::string& numbers);
 
 private:
-    std::vector<int> split(const std::string& numbers, const std::string& delimiter);
     std::string extractDelimiter(std::string& numbers);
+    std::vector<std::string> split(const std::string& str, const std::string& delimiter);
     std::vector<int> checkForNegatives(const std::vector<int>& numbers); // Pure function
     int ignoreNumbersGreaterThan1000(const std::vector<int>& numbers);
     std::string formatNegatives(const std::vector<int>& negatives); // New helper function
