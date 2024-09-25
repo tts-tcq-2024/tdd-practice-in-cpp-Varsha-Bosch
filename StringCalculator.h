aab94ccceb93e895904f6171cd02c3a31a0d9c13
Varsha-Bosch <varshasrinivas46@gmail.com>
2024-09-25 09:56:53 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,20 @@
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+
+private:
+    std::vector<int> split(const std::string& numbers, const std::string& delimiter);
+    std::string extractDelimiter(std::string& numbers);
+};
+
+#endif // STRINGCALCULATOR_H
 
